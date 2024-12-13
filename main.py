b451032ff08c913e733a9e3eb96389f98576e65c
Cocu Matei-Iulian <98695197+mateipiratul@users.noreply.github.com>
2024-12-13 09:49:21 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import re
 
-def format_html(file):
+
+def format_html(file_path):
     try:
         with open(file_path, 'r', encoding="utf-8") as file:
             lines = file.readlines()
+
         indent_level = 0
         formatted_lines = []
+        taguri_self_closing = ["area", "base", "br", "col", "embed", "hr", "img", "input",
+                               "link", "meta", "param", "source", "track", "wbr", "frame",
+                               "command", "keygen", "menuitem"]
 
         for line in lines:
             line = line.strip()
 
             if not line:
                 continue  # Sarim peste liniile goale
+
+            # Verificam daca linia este un tag self-closing
+            is_self_closing = any(
+                re.match(fr'<{tag}[^>]*>$', line) for tag in taguri_self_closing
+            )
+
+            if is_self_closing:
+                # Adaugam linia fara a schimba nivelul de indentare
+                formatted_lines.append('    ' * indent_level + line)
+                continue
 
             # Detectam tag-urile de inchidere
             if re.match(r'</[^>]+>', line):
@@ -36,7 +51,7 @@ def format_html(file):
     except Exception as e:
         print(f"A aparut o eroare: {e}")
 
+
 if __name__ == '__main__':
     file_path = "index.html"
-    f = open(file_path)
-    format_html(f)
+    format_html(file_path)
